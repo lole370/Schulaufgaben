f570c0845ca8343a44134390a4205ce233208aa9
Ole Lorbacher <ole.lorbacher@sap.com>
2022-10-18 09:41:19 +0200

    initial commit

---
+++ src/aufgabenBlock1/ThreeTwoOne.java
@@ -0,0 +1,12 @@
+package aufgabenBlock1;
+
+public class ThreeTwoOne {
+
+	public static void main(String[] args) {
+		System.out.println("Three");
+		System.out.println("Two");
+		System.out.print("One");
+		System.out.println(" - Takeoff!");
+	}
+
+}
