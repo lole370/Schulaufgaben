40f21f468d6160c3d51df4c257dbcdb3f41ea7ca
Ole Lorbacher <ole.lorbacher@sap.com>
2022-10-18 11:45:32 +0200

    refactored LKW-Maut

--- src/aufgabenBlock2/LKWMaut.java
+++ src/aufgabenBlock2/LKWMaut.java
@@ -10,16 +10,9 @@ public class LKWMaut {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Geben Sie die gefahrenen Kilometer ein: ");
 		int distance = sc.nextInt();
-		String envClass = "";
-		Map<String, Double[]> envClasses = new HashMap<String, Double[]>();
-		envClasses.put("A", new Double[] { 12.5d, 13.1d });
-		envClasses.put("B", new Double[] { 14.6d, 15.2d });
-		envClasses.put("C", new Double[] { 15.7d, 16.3d });
-		envClasses.put("D", new Double[] { 18.8d, 19.4d });
-		envClasses.put("E", new Double[] { 19.8d, 20.4d });
-		envClasses.put("F", new Double[] { 20.8d, 21.4d });
-
-		while (!envClasses.containsKey(envClass)) {
+		String envClass = " ";
+		
+		while (!"ABCDEF".contains(envClass)) {
 			System.out.print("Geben Sie die gefahrenen Schadstoffklasse (A,B,C,D,E,F) ein: ");
 			envClass = sc.next();
 		}
@@ -27,11 +20,18 @@ public class LKWMaut {
 		System.out.print("Geben Sie die Anzahl der Axen ein: ");
 		int axis = sc.nextInt();
 		sc.close();
-		printMaut(distance, axis, envClass, envClasses);
+		printMaut(distance, axis, envClass);
 
 	}
 
-	public static void printMaut(int distance, int axis, String envClass, Map<String, Double[]> envClasses) {
+	public static void printMaut(int distance, int axis, String envClass) {
+		Map<String, Double[]> envClasses = new HashMap<String, Double[]>();
+		envClasses.put("A", new Double[] { 12.5d, 13.1d });
+		envClasses.put("B", new Double[] { 14.6d, 15.2d });
+		envClasses.put("C", new Double[] { 15.7d, 16.3d });
+		envClasses.put("D", new Double[] { 18.8d, 19.4d });
+		envClasses.put("E", new Double[] { 19.8d, 20.4d });
+		envClasses.put("F", new Double[] { 20.8d, 21.4d });
 		double maut = 0;
 		if (axis <= 3) {
 			maut = distance * envClasses.get(envClass)[0];
