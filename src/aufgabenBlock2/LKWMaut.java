a09db9330ce53de163879feba65c408514f15fd8
Ole Lorbacher <ole.lorbacher@sap.com>
2022-10-20 10:25:02 +0200

    formatted LKW-Maut

--- src/aufgabenBlock2/LKWMaut.java
+++ src/aufgabenBlock2/LKWMaut.java
@@ -11,7 +11,7 @@ public class LKWMaut {
 		System.out.print("Geben Sie die gefahrenen Kilometer ein: ");
 		int distance = sc.nextInt();
 		String envClass = " ";
-		
+
 		while (!"ABCDEF".contains(envClass)) {
 			System.out.print("Geben Sie die gefahrenen Schadstoffklasse (A,B,C,D,E,F) ein: ");
 			envClass = sc.next();
@@ -32,8 +32,9 @@ public class LKWMaut {
 		envClasses.put("D", new Double[] { 18.8d, 19.4d });
 		envClasses.put("E", new Double[] { 19.8d, 20.4d });
 		envClasses.put("F", new Double[] { 20.8d, 21.4d });
-		double maut = distance * envClasses.get(envClass)[axis < 4 ? 0: 1];	
-		System.out.format("Schadstoffklasse: %s, %d Achsen, %d Kilometer, -> %.2f Eurocent", envClass, axis, distance, maut);
+		double maut = distance * envClasses.get(envClass)[axis < 4 ? 0 : 1];
+		System.out.format("Schadstoffklasse: %s, %d Achsen, %d Kilometer, -> %.2f Eurocent", envClass, axis, distance,
+				maut);
 	}
 
 }
