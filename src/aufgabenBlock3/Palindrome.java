f8e3a34208138c9b5414eafdfa4dd8bfe8ffb643
Ole Lorbacher <ole.lorbacher@sap.com>
2022-11-17 14:26:07 +0100

    rework palindrome

--- src/aufgabenBlock3/Palindrome.java
+++ src/aufgabenBlock3/Palindrome.java
@@ -1,10 +1,7 @@
 package aufgabenBlock3;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,38 +14,56 @@ public class Palindrome {
 		String input = sc.next();
 		sc.close();
 
-		Map<String, String> palindromes = new HashMap<String, String>();
-		for (String w : getWords(input)) {
-			if (isPalindrome(w)) {
-				palindromes.put(w, getReversed(w));
-			}
-		}
-		System.out.println("Palindromes:");
-		for (Entry<String, String> p : palindromes.entrySet()) {
-			System.out.format("%s\t|\t%s\n", p.getKey(), p.getValue());
-		}
-	}
+		System.out.println("Palindrome:");
 
-	public static boolean isPalindrome(String str) {
-		if (str.toLowerCase().equals(getReversed(str.toLowerCase()))) {
-			return true;
-		}
-		return false;
-
-	}
-
-	public static List<String> getWords(String s) {
-		List<String> allMatches = new ArrayList<String>();
-		Matcher m = Pattern.compile("[\\pL]+",
-				Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(s);
-		while (m.find()) {
-			allMatches.add(m.group());
-		}
-		return allMatches;
+		printMapAsTable(getPalindromes(input));
 	}
 
 	public static String getReversed(String s) {
 		return new StringBuilder(new String(s.toCharArray())).reverse().toString();
 
 	}
+
+	public static String getIfPalindrome(String str) {
+		String reversed = getReversed(str);
+		if (str.toLowerCase().equals(reversed.toLowerCase())) {
+			return reversed;
+		}
+		return null;
+
+	}
+
+	public static Map<String, String> getPalindromes(String s) {
+		Map<String, String> palindromes = new HashMap<String, String>();
+		Matcher m = Pattern
+				.compile("[\\pL]+", Pattern.UNICODE_CHARACTER_CLASS | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE)
+				.matcher(s);
+		while (m.find()) {
+			String w = m.group();
+			palindromes.put(w, getIfPalindrome(w));
+		}
+		return palindromes;
+	}
+
+	public static void printMapAsTable(Map<String, String> map) {
+		if (map.isEmpty())
+			return;
+		int maxLen = 0;
+		for (String w : map.keySet()) {
+			if (w.length() > maxLen) {
+				maxLen = w.length();
+			}
+		}
+		String line = "═".repeat(maxLen + 4);
+		String singleLine = "─".repeat(maxLen + 4);
+		System.out.format("╔%s╦%s╗%n", line, line);
+		for (int i = 0; i < map.size(); i++) {
+			System.out.format("║  %-" + maxLen + "s  ║  %-" + maxLen + "s  ║%n", map.keySet().toArray()[i],
+					map.values().toArray()[i]);
+			if (i != map.size() - 1)
+				System.out.format("╟%s╫%s╢%n", singleLine, singleLine);
+		}
+		System.out.format("╚%s╩%s╝%n", line, line);
+	}
+
 }
